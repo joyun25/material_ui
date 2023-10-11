6e299f25229d2b88778a1a7ee671744606fdfe6f
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-11 11:25:41 +0800

    add icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Typography, Button, Container } from '@mui/material'
+// import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined'
+// import SendIcon from '@mui/icons-material/Send'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
 
 export default function Create() {
   return (
@@ -19,9 +22,20 @@ export default function Create() {
           color="secondary"
           variant="contained"
           onClick={() => console.log('you clicked me')}
+          // startIcon={<SendIcon/>}
+          endIcon={<KeyboardArrowRightIcon/>}
         >
           Submit
         </Button>
+
+        {/* icons */}
+        {/* <AcUnitOutlinedIcon/>
+        <AcUnitOutlinedIcon color="secondary" fontSize="large"/>
+        <AcUnitOutlinedIcon color="secondary" fontSize="small"/>
+        <AcUnitOutlinedIcon color="action" fontSize="small"/>
+        <AcUnitOutlinedIcon color="error" fontSize="small"/>
+        <AcUnitOutlinedIcon color="disabled" fontSize="small"/>
+        <br /> */}
       </Container>
     </div>
   )
