918b97986c5beb2de51b1b5903a80da7f4d181de
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-11 11:16:35 +0800

    add Button and Container

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Typography, Button, Container } from '@mui/material'
 
 export default function Create() {
   return (
     <div>
-      <Typography
-        variant="h6"
-        component="h2"
-        color="textSecondary"
-        gutterBottom
-      >
-        Create a New Note
-      </Typography>
+      <Container>
+        <Typography
+          variant="h6"
+          component="h2"
+          color="textSecondary"
+          gutterBottom
+        >
+          Create a New Note
+        </Typography>
+
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          onClick={() => console.log('you clicked me')}
+        >
+          Submit
+        </Button>
+      </Container>
     </div>
   )
 }
