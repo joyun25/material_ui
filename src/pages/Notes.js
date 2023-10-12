7ded50fa5e5a04126a6c0e6884cc4a557193c73a
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-12 09:21:35 +0800

    add Mosonry CSS

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import { Container } from '@material-ui/core'
 import NoteCard from '../components/NoteCard'
+import Masonry from 'react-masonry-css'
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -22,15 +23,25 @@ export default function Notes() {
     setNotes(newNotes);
   }
 
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
+  }
+
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid-column"
+      >
         {notes.map(note => (
-          <Grid item key={note.id} xs={12} sm={6} md={3}>
+          <div key={note.id}>
             <NoteCard note={ note } handleDelete={handleDelete}/>
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   )
 }
